d6bf487c5cdc43bc248cc69341f4512c37521629
Fabio Di Michele <webmaster@icomed.it>
2021-11-28 16:38:36 +0100

    Rename
    Rename the lib files in uppercase

---
+++ KIMlib.cpp
@@ -0,0 +1,448 @@
+/*
+    kimlib.cpp - Library for KNX module (KIM)
+    Copyright (C) 2021  Fabio Di MIchele
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <Arduino.h>
+#include <KIMlib.h>
+#include <Wire.h>
+
+//**************************** Routine di interrupt su fronte di salita del dato disponibile ******************************
+volatile boolean kimBufFull = false;
+
+static void kimDataRedyHandler() 
+{
+  kimBufFull = true;
+}
+   
+// CONTRUCTORS
+KIMaip::KIMaip(uint8_t pinDataReady, uint8_t pinKNXbus)
+{
+  Wire.begin();                        // Inizializza I2C
+  pinMode(pinKNXbus, INPUT_PULLUP);    // Pin 14 Gateway KNX
+  pinMode(pinDataReady, INPUT_PULLUP); // Pin 1 Gateway KNX      
+  attachInterrupt(digitalPinToInterrupt(pinDataReady), kimDataRedyHandler, RISING);        // abilitazione funzione interrupt su fronte di salita pin 2 
+  _pinBusKNX = pinKNXbus;
+}
+    
+void KIMaip::setDevID(int x)
+{
+  _idDev = x;
+}
+
+int KIMaip::getDevID()
+{
+  return _idDev;
+}
+  
+boolean KIMaip::isReadBusy()
+{
+  if (_idDPTRead != -1) return true;
+  else return false;  
+}
+
+void KIMaip::setIdDPTRead(int x)
+{ 
+  _idDPTRead = x;    
+}
+
+void KIMaip::startReadTimeout()
+{
+  _readTimeout = millis();
+}
+
+int KIMaip::getIdDPTRead()
+{
+  return  _idDPTRead; 
+}
+    
+boolean KIMaip::recive()
+{      
+  unsigned long stopWatch = millis();
+	
+  if ((millis() - _readTimeout) >  KIM_READ_TIMEOUT) _idDPTRead = -1;
+  
+  if (this->isBusKNXready()) 
+  {	 
+	while( (millis() - stopWatch) < 50) 
+	{ // Timeout di attesa risposta
+	  if (kimBufFull)
+	  {
+		Wire.requestFrom(_idDev, 40);      // request 40 bytes from slave device DevId
+		_rxLength = Wire.read();           // receive Length
+		_rxCommand = Wire.read();              // receive SendCommand
+		for (int i=0; i < (_rxLength-(1)); i++)
+		{
+		  buf[i] = Wire.read();        // receive next byte of telegram and load Array Buffer
+		}
+		_rxDato = int(buf[0]*256) + int(buf[1]);   // Azioni per il valore ricevuto di ciascun oggetto
+		_rxValue = int(buf[2]*256) + int(buf[3]);
+		kimBufFull = false;       // Reset Flag
+		return true;
+	  }
+	}
+	return false;
+  }
+}
+
+unsigned long KIMaip::getSystemEvent()
+{
+  union l_tag {                             // Siccome la temperatura è un dato FLOAT, si usa la funzione Union per "impacchettare" i 4 BYTE che la compongono.
+	unsigned long temp_long;                // Se copi dentro "u.temp_float" un valore float automaticamente ottieni i relativi quattro byte nel array "u.temp_byte[n]", con n compreso tra 0 e 3,
+	byte temp_byte[4];                      // Viceversa se copy nel array i quattro byte, ottieni il tuo valore float dentro u.temp_float.
+  } l;
+  int y = 3;
+  if (_rxCommand == SYSTEM_EVENT) {
+	for (int i=0; i < 4; i++){
+	  l.temp_byte[y--] = buf[i];
+	}
+  }
+  return l.temp_long;
+}
+
+String KIMaip::getVersion()
+{
+  String str = "";
+  this->readSystemParameter(KIM_VERSION);
+  delay(KIM_WAIT);
+  if (this->recive()) {
+	if (_rxDato == KIM_VERSION) {
+	  for (int i=0; i < (_rxLength-3); i++){
+		if (i > 0) str += F(".");
+		str += buf[i+2];            
+	  }
+	}
+  }
+  return str;
+}
+
+word KIMaip::getKNXaddress()
+{
+  union w_tag {                             // Siccome la temperatura è un dato FLOAT, si usa la funzione Union per "impacchettare" i 4 BYTE che la compongono.
+	word temp_word;                         // Se copi dentro "u.temp_float" un valore float automaticamente ottieni i relativi quattro byte nel array "u.temp_byte[n]", con n compreso tra 0 e 3,
+	byte temp_byte[2];                      // Viceversa se copy nel array i quattro byte, ottieni il tuo valore float dentro u.temp_float.
+  } w;
+  int len = _rxLength - 3;
+  this->readSystemParameter(KIM_KNX_ADDRESS);
+  delay(KIM_WAIT);
+  if (this->recive()) {
+	if (_rxDato == KIM_KNX_ADDRESS) {
+	  for (int i=0; i < len; i++){
+		if (i < 2) w.temp_byte[i] = buf[i+2];
+	  }
+	}
+  }
+  return w.temp_word;
+}
+
+byte KIMaip::getProgMode()
+{
+  this->readSystemParameter(KIM_PROG_MOD);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_PROG_MOD) {
+	  return buf[2];
+	}
+  }
+  return 0;
+}
+
+byte KIMaip::getI2Caddress()
+{
+  this->readSystemParameter(KIM_I2C_ADDRESS);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_I2C_ADDRESS) {
+	  return buf[2];
+	}
+  }
+  return 0;
+}
+
+byte KIMaip::getI2CacutalAddress()
+{
+  this->readSystemParameter(KIM_ACTUAL_I2C);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_ACTUAL_I2C) {
+	  return buf[2];
+	}
+  }
+  return 0;
+}
+
+unsigned long KIMaip::getSYSevent()
+{
+  union l_tag {                             // Siccome la temperatura è un dato FLOAT, si usa la funzione Union per "impacchettare" i 4 BYTE che la compongono.
+	unsigned long temp_long;                // Se copi dentro "u.temp_float" un valore float automaticamente ottieni i relativi quattro byte nel array "u.temp_byte[n]", con n compreso tra 0 e 3,
+	byte temp_byte[4];                      // Viceversa se copy nel array i quattro byte, ottieni il tuo valore float dentro u.temp_float.
+  } l;
+  int len = _rxLength - 3;
+  this->readSystemParameter(KIM_SYS_EVENT);
+  delay(KIM_WAIT);
+  if (this->recive()) {
+	if (_rxDato == KIM_SYS_EVENT) {
+	  for (int i=0; i < len; i++){
+		if (i < 4) l.temp_byte[i] = buf[i+2];
+	  }
+	}
+  }
+  return l.temp_long;
+}
+
+byte KIMaip::getEnableError()
+{
+  this->readSystemParameter(KIM_EN_ERROR);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_EN_ERROR) {
+	  return buf[2];
+	}
+  }
+  return 0;
+}
+
+unsigned int KIMaip::getI2Ctimeout()
+{
+  this->readSystemParameter(KIM_I2C_TIMEOUT);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_I2C_TIMEOUT) {
+	  return _rxValue;
+	}
+  }
+  return 0;
+}
+
+unsigned int KIMaip::getIndTimeout()
+{
+  this->readSystemParameter(KIM_IND_TIMEOUT);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_IND_TIMEOUT) {
+	  return _rxValue;
+	}
+  }
+  return 0;
+}
+
+unsigned int KIMaip::getMasterTimeout()
+{
+  this->readSystemParameter(KIM_MASTER_TIMEOUT);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_MASTER_TIMEOUT) {
+	  return _rxValue;
+	}
+  }
+  return 0;
+}
+
+byte KIMaip::getRtySend()
+{
+  this->readSystemParameter(KIM_RTY_SEND);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_RTY_SEND) {
+	  return buf[2];
+	}
+  }
+  return 0;
+}
+
+byte KIMaip::getTransMode()
+{
+  this->readSystemParameter(KIM_TRANS_MOD);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_TRANS_MOD) {
+	  return buf[2];
+	}
+  }
+  return 0;
+}
+
+byte KIMaip::getGroupLink()
+{
+  this->readSystemParameter(KIM_GROUP_LINK);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_GROUP_LINK) {
+	  return buf[2];
+	}
+  }
+  return 0;
+}
+
+byte KIMaip::getDevStatus()
+{
+  this->readSystemParameter(KIM_DEV_STATUS);
+  delay(KIM_WAIT);
+  if (this->recive()) {        
+	if (_rxDato == KIM_DEV_STATUS) {
+	  return buf[2];
+	}
+  }
+  return 0;
+}
+
+byte KIMaip::getRXcommand()
+{
+  return _rxCommand;
+}
+
+byte KIMaip::getRXdato()
+{
+  return _rxDato;
+}
+
+byte KIMaip::getRXvalue()
+{
+  return _rxValue;
+}
+
+byte KIMaip::addUserParameter()
+{
+  return _idxUserParameter++;
+}
+ 
+boolean KIMaip::isBusKNXready()
+{
+  return digitalRead(_pinBusKNX);
+}
+
+void KIMaip::readSystemParameter(int ParId) 
+{
+  // step 1: instruct device to SYSTEM PARAMETER READ
+  Wire.beginTransmission(_idDev);  // transmit to device DevId (0x08)
+  Wire.write(byte(0x03));         // sets Length (0x03)
+  Wire.write(byte(SYS_PARAMETER_READ));         // sets Command for Syst Param Read: (0x61)
+  Wire.write(highByte(ParId));    // Syst Param-IDS 1/2
+  Wire.write(lowByte(ParId));     // Syst Param-IDS 2/2
+  Wire.endTransmission();         // stop transmitting
+  kimBufFull = false;             // Reset Flag
+}
+
+//**************************** DPT ******************************    
+
+/* *************************************  Invia un valore ad un oggetto   ***********************************************************************
+*  
+*  Comando per inviare i valori dell'Buffer di dimensione nr all'oggetto obj 
+*  
+*************************************************************************************************************************************************  */
+void KIMaip::sendValue(int obj, byte nr) {
+  
+	// step 1: instruct device to OBJECT SEND VALUE
+	Wire.beginTransmission(_idDev);  // transmit to device DevId (0x08)
+	Wire.write(byte(0x03+nr));      // sets Length (0x03) + nr
+	Wire.write(byte(VALUE_SEND));   // sets Command for Send Value: (0x10)
+	Wire.write(highByte(obj));      // Obj-IDS 1/2
+	Wire.write(lowByte(obj));       // Obj-IDS 2/2
+	for (int i=0; i < nr; i++){
+	  Wire.write(byte(buf[i]));     // Object value
+	}
+	Wire.endTransmission();         // stop transmitting
+	kimBufFull = false;             // Reset Flag
+}
+
+/* *************************************  Risponde ad una richiesta di lettura da parte dal BUS KNX verso un oggetto  ***************************
+*  
+*  Comando per inviare i valori dell'Buffer di dimensione nr all'oggetto obj in risposta ad una richiesta di lettura pervenuta dal BUS KNX
+*  
+*************************************************************************************************************************************************  */
+void KIMaip::sendResponseValue(int obj, byte nr) {
+
+	// step 1: instruct device to OBJECT SEND RESPONSE VALUE
+	Wire.beginTransmission(_idDev);  // transmit to device DevId (0x08)
+	Wire.write(byte(0x03+nr));      // sets Length (0x03) + nr
+	Wire.write(byte(VALUE_RESPONSE_SEND));         // sets Command for Send Value: (0x12)
+	Wire.write(highByte(obj));      // Obj-IDS 1/2
+	Wire.write(lowByte(obj));       // Obj-IDS 2/2
+	for (int i=0; i < nr; i++){
+	  Wire.write(byte(buf[i]));  // Object value
+	}
+	Wire.endTransmission();         // stop transmitting
+	kimBufFull = false;             // Reset Flag
+}
+
+/* *************************************  Invia una richiesta di leggere un valore dal BUS KNX ad un oggetto   ***********************************
+*  
+*  Comando per inviare la richiesta di lettura dal BUS KNX all'oggetto obj
+*  Il dispositvo sul BUS KNX che è abilitato a rispondere, invierà il valore all'indirizzo di gruppo deve è contenuto obj
+*  
+*************************************************************************************************************************************************  */
+void KIMaip::sendReadValue(int obj) {
+
+	// step 1: instruct device to OBJECT SEND READ VALUE
+	Wire.beginTransmission(_idDev);  // transmit to device DevId (0x08)
+	Wire.write(byte(0x03));         // sets Length (0x03)
+	Wire.write(byte(VALUE_READ_SEND));         // sets Command for SendReadValue: (0x11)
+	Wire.write(highByte(obj));      // Obj-IDS 1/2
+	Wire.write(lowByte(obj));       // Obj-IDS 2/2
+	Wire.endTransmission();         // stop transmitting
+	kimBufFull = false;             // Reset Flag
+}
+
+// *************************************  User Parameter   ***********************************
+// CONTRUCTORS
+UserParameter::UserParameter(KIMaip *kimid)
+{
+  _value = 0;
+  _idx = kimid->addUserParameter();
+  //_idx = id;
+  _kimaid = kimid;
+}
+
+void UserParameter::setValue(uint8_t x)
+{
+  _value = x;
+}
+
+uint8_t UserParameter::getValue()
+{  
+  this->readParameter(_idx, 1);
+  delay(KIM_WAIT);
+  if (_kimaid->recive()) { 
+	if (_kimaid->getRXcommand() == PARAMETER_RESPONSE) {
+		_value = _kimaid->buf[3];  
+	}
+  }
+  return _value;
+}
+    
+int UserParameter::index()
+{
+  return _idx;
+}
+  
+/* *************************************  Legge i parametri   ***********************************************************************************
+ *  
+ *  Comando per leggere nr parametri (max 96 bytes) a partire dall'indirizzo address 
+ *  
+ *************************************************************************************************************************************************  */
+      
+void UserParameter::readParameter(int address, byte nr) {
+
+	// step 1: instruct device to  PARAMETER READ
+	Wire.beginTransmission(_kimaid->getDevID());  // transmit to device DevId (0x08)
+	Wire.write(byte(0x04));             // sets Length (0x04)
+	Wire.write(byte(PARAMETER_READ));   // sets Command for Param Read: (0x41)
+	Wire.write(highByte(address));      // Param-IDS 1/2
+	Wire.write(lowByte(address));       // Param-IDS 2/2
+	Wire.write(nr);                     // Nr Param
+	Wire.endTransmission();             // stop transmitting
+	kimBufFull = false;                 // Reset Flag
+}
---
+++ KIMlib.h
@@ -0,0 +1,217 @@
+/*
+    kimlib.h - Library for KNX module (KIM)
+    Copyright (C) 2021  Fabio Di MIchele
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef KIMlib_h
+#define KIMlib_h
+
+#include <Arduino.h>
+
+#define KIM_DEFAULT_ID    8     // Indirizzo I2C del Gateway KNX
+#define KIM_BUFFER_LENGTH 96
+#define KIM_WAIT          5
+#define KIM_READY         0x83
+#define KIM_READ_TIMEOUT  1000.0
+
+// Messages al Module
+enum MessageToKIM {
+  VALUE_SEND          = 0x10,
+  VALUE_READ_SEND     = 0x11,
+  VALUE_RESPONSE_SEND = 0x12,
+  PARAMETER_READ      = 0x41,
+  SYS_PARAMETER_READ  = 0x61
+};
+
+// Messages dal Module
+enum MessageFromKIM {
+  VALUE_RECEIVE       = 0x20,
+  VALUE_READ_RECEIVE  = 0x21,
+  PARAMETER_RESPONSE  = 0x42,
+  SYSTEM_EVENT        = 0x80
+};
+
+static void kimDataRedyHandler(); //prototype for interrupt handler
+
+class KIMaip
+{
+  public:
+	byte buf[KIM_BUFFER_LENGTH] = {};
+	
+    KIMaip(uint8_t pinDataReady, uint8_t pinKNXbus);
+    
+    boolean recive();
+    boolean isReadBusy();
+    boolean isBusKNXready();
+    void startReadTimeout();
+
+    void setDevID(int x);
+    int getDevID();
+
+    void setIdDPTRead(int x);
+    int getIdDPTRead();
+
+    unsigned long getSystemEvent();
+    String getVersion();
+    word getKNXaddress();
+    byte getProgMode();
+    byte getI2Caddress();
+    byte getI2CacutalAddress();
+    unsigned long getSYSevent();
+    byte getEnableError();
+    unsigned int getI2Ctimeout();
+    unsigned int getIndTimeout();
+    unsigned int getMasterTimeout();
+    byte getRtySend();
+    byte getTransMode();
+    byte getGroupLink();
+    byte getDevStatus();
+    byte getRXcommand();
+    byte getRXdato();
+    byte getRXvalue();
+	
+	void sendValue(int obj, byte nr);
+	void sendResponseValue(int obj, byte nr);
+	void sendReadValue(int obj);
+	
+    byte addUserParameter();
+ 
+ protected:
+	void readSystemParameter(int ParId);	
+	
+ private: 
+    byte _pinBusKNX = 0;
+    byte _rxLength = 0;
+    int _idDev = KIM_DEFAULT_ID;
+    byte _rxCommand = 0;
+    int _rxDato = 0;
+    int _rxValue = 0;
+    byte _idxUserParameter = 0;
+    int _idDPTRead = -1;
+    long _readTimeout = 0;
+};
+
+  /* *************************************  Legge i parametri di sistema   *************************************************************************
+   *  
+   *  Comando per leggere i parametri del sistema
+   *  
+   *************************************************************************************************************************************************  */
+enum CmdKIM {
+  KIM_VERSION        = 0x0001,  // Version
+  KIM_KNX_ADDRESS    = 0x0002,  // Physical address
+  KIM_PROG_MOD       = 0x0003,  // Programming mode
+  KIM_I2C_ADDRESS    = 0x0005,  // I²C-Address, see Chap. 7.8.3.1
+  KIM_ACTUAL_I2C     = 0x0006,  // Actual used I²C-Address, see Chap. 7.8.3.1
+  KIM_SYS_EVENT      = 0x0011,  // Bit field to enable the System-Events
+  KIM_EN_ERROR       = 0x0012,  // Enable error-messages
+  KIM_I2C_TIMEOUT    = 0x0020,  // Timeout over an I²C-message
+  KIM_IND_TIMEOUT    = 0x0021,  // Timeout for indications
+  KIM_MASTER_TIMEOUT = 0x0022,  // Timeout for indications if no communication to the master
+  KIM_RTY_SEND       = 0x0023,  // Retry sending indications, till no communication is detected
+  KIM_TRANS_MOD      = 0x0100,  // Settings for transparent-mode
+  KIM_GROUP_LINK     = 0x0110,  // GroupLink disabled
+  KIM_DEV_STATUS     = 0x0200,  // State of Device, see Chap. 7.8.3.2
+  KIM_DEV_RESET      = 0xF0F0   // Reset the module
+};
+
+//template<class T>
+class UserParameter
+{
+	public:
+		UserParameter(KIMaip *kimid);
+		void setValue(uint8_t x);
+		uint8_t getValue();
+		int index();
+    
+	protected:
+		void readParameter(int address, byte nr);
+		
+	private:
+		uint8_t _idx;
+		uint8_t _value;
+		KIMaip *_kimaid;
+};
+
+class DPT
+{
+	public:
+		DPT(uint8_t x, KIMaip *kimid)
+		{
+			_id = x;
+			_kimaid = kimid;
+		}
+    
+		template<class T>
+		void setValue(T& value)
+		{
+			const byte* p = (const byte*)(const void*)&value;
+			uint8_t len = sizeof(T);
+			int y = len - 1;
+			for (int i=0; i < len; i++) {
+				_kimaid->buf[i] = ((byte *) p)[y--];
+			}
+			_kimaid->sendValue(_id, len);
+		}
+		template<class T>
+		void getValue(T& value)
+		{
+            byte* p = (byte*)(void*)&value;
+            uint8_t len = sizeof(T);
+            int y = len + 1;
+            if (_kimaid->getRXdato() == _id) {
+                switch (_kimaid->getRXcommand()) {
+                  case VALUE_RECEIVE:
+                    if (_id == _kimaid->getIdDPTRead()) _kimaid->setIdDPTRead(-1);
+                    for (int i=0; i < len; i++) {
+				  *p++ = _kimaid->buf[y--];
+				}
+				break;
+			}        
+		  }
+		}
+    
+		template<class T>
+		void responseValue(T& value)
+		{
+			const byte* p = (const byte*)(const void*)&value;
+			if (_kimaid->getRXdato() == _id) {   
+				switch (_kimaid->getRXcommand()) {
+				  case VALUE_READ_RECEIVE:
+					uint8_t len = sizeof(T);
+					int y = len - 1;
+					for (int i=0; i < len; i++) {
+						_kimaid->buf[i] = ((byte *) p)[y--];
+					}
+					_kimaid->sendResponseValue(_id, sizeof(T)); 
+					break;
+				}
+			}
+		}
+		
+		void getStatusValue()
+		{			
+		  if (!_kimaid->isReadBusy()) {    
+			_kimaid->sendReadValue(_id);
+			_kimaid->setIdDPTRead(_id);
+			_kimaid->startReadTimeout();
+		  }
+		}
+		
+	private:
+		byte _id;
+		KIMaip *_kimaid;
+};
+#endif
